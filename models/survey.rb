8421619177eb0a1b04180d141e5cdd3a8bd5b416
Gaaston12 <gaastonmaartin12@gmail.com>
2021-05-23 20:06:18 -0300

    creation of metod validate for survey

--- models/survey.rb
+++ models/survey.rb
@@ -1,4 +1,9 @@
 class Survey < Sequel::Model
     one_to_many :responses
     many_to_one :career
+
+    def validate
+        super
+        errors.add(:username, 'cannot be empty') if !username || username.empty?
+    end
 end
