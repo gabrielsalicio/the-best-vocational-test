fa2a072eb704036f7e5173e67b075b239696acd8
Gaaston12 <gaastonmaartin12@gmail.com>
2021-05-27 17:57:51 -0300

    creation of models for the tests

--- models/choice.rb
+++ models/choice.rb
@@ -2,4 +2,11 @@ class Choice < Sequel::Model
     one_to_one :response
     many_to_one :question
     one_to_many :outcomes
+
+
+    def validate 
+        super
+        errors.add(:text, :text => 'cant be nil or empty') if text.nil? || text.empty?
+    
+    end
 end
