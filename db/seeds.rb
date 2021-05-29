d7e73b1291806e60f16a6d0d27c371d459faa44d
Gaaston12 <gaastonmaartin12@gmail.com>
2021-05-29 19:06:16 -0300

    load all careers

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,22 @@ Career.all.map { |c| c.destroy }
 
 
 #Create Career
-computacion = Career.create(name: 'Computacion')
+agronomia = Career.create(name: 'Agronomia')
+arquitectura = Career.create(name: 'Arquitectura')
+arte = Career.create(name: 'Arte')
+computación = Career.create(name: 'Computación')
+economía = Career.create(name: 'Economía')
+educación_física = Career.create(name: 'Educación física')
+filosofía = Career.create(name: 'Filosofía')
+física = Career.create(name: 'Física')
+geología = Career.create(name: 'Geología')
+historia = Career.create(name: 'Historia')
+literaruta = Career.create(name: 'Literaruta')
+matemáticas = Career.create(name: 'Matemáticas')
+medicina = Career.create(name: 'Medicina')
+periodismo = Career.create(name: 'Periodismo')
+química = Career.create(name: 'Química')
+sociología = Career.create(name: 'Sociología')
 veterinaria = Career.create(name: 'Veterinaria')
 
 #Create surveys example
