b937f80fef3a85fcea831275c50d19fada7f8aa9
camilo-pg <camilopetrignanigorla@gmail.com>
2021-06-09 23:58:59 -0300

    Preguntas 12,13 y 15 y modificacion en las preguntas ya creadas

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ veterinaria      = Career.create(name: 'Veterinaria', description:'', duration: 
 ques1 = Question.create(name: 'Pregunta N°1', 
     description:'Me trasladaría a una zona agrícola-ganadera para ejercer mi profesión.', 
     number: 1, 
-    type: 'Boolean')
+    type: 'Integer')
 ques1.save
     #Create Choice
 	choice1_q1 = Choice.create(text:'1- SI', question:ques1)
@@ -48,7 +48,7 @@ ques1.save
 ques2 = Question.create(name:'Pregunta N°2', 
     description:'Tengo buena memoria y no me cuesta estudiar y retener fórmulas y palabras técnicas.', 
     number: 2, 
-    type: 'Boolean')
+    type: 'Integer')
 ques2.save
     choice1_q2 = Choice.create(text:'1- SI', question:ques2)
         Outcome.create(choice: choice1_q2, career: fisica)
@@ -60,7 +60,7 @@ ques2.save
 ques3 = Question.create(name: 'Pregunta N°3', 
     description:'Me gusta escribir. En general mis trabajos son largos y están bien organizados.', 
     number: 3, 
-    type: 'Boolean')
+    type: 'Integer')
 ques3.save    
     choice1_q3 = Choice.create(text: '1- SI', question:ques3)
         Outcome.create(choice: choice1_q3, career: literaruta)
@@ -70,7 +70,7 @@ ques3.save
 ques4 = Question.create(name: 'Pregunta N°4', 
     description:'Sé quien es Steven Hawking: Conozco y me atraen sus descubrimientos.', 
     number: 4, 
-    type: 'Boolean')
+    type: 'Integer')
 ques4.save    
     choice1_q4 = Choice.create(text: '1- SI', question:ques4)
         Outcome.create(choice: choice1_q4, career: fisica)
@@ -79,7 +79,7 @@ ques4.save
 ques5 = Question.create(name: 'Pregunta N°5', 
     description:'Me actualizo respecto de los avances tecnológicos y me intereso por investigar y conocer', 
     number: 5, 
-    type: 'Boolean')
+    type: 'Integer')
 ques5.save
     choice1_q5 = Choice.create(text: '1- SI', question:ques5)
         Outcome.create(choice: choice1_q5, career: computacion)
@@ -88,7 +88,7 @@ ques5.save
 ques6 = Question.create(name: 'Pregunta N°6', 
     description:'Sé quien es Andy Warhol y a qué movimiento artístico pertenece. Me apasiona conocer acerca del arte y sus exponentes.', 
     number: 6, 
-    type: 'Boolean')
+    type: 'Integer')
 ques6.save
     choice1_q6 = Choice.create(text: '1- SI', question:ques6)
         Outcome.create(choice: choice1_q6, career: arte)
@@ -97,7 +97,7 @@ ques6.save
 ques7 = Question.create(name: 'Pregunta N°7', 
     description:'En general me intereso por las dificultades que ha tenido que atravesar la humanidad y cómo lo ha superado.', 
     number: 7, 
-    type: 'Boolean')
+    type: 'Integer')
 ques7.save
     choice1_q7 = Choice.create(text: '1- SI', question:ques7)
         Outcome.create(choice: choice1_q7, career: historia)
@@ -106,7 +106,7 @@ ques7.save
 ques8 = Question.create(name: 'Pregunta N°8', 
     description:'Me apasiona leer y no me importa si el libro que elijo tiene muchas páginas,para mí es un gran entretenimiento.', 
     number: 8, 
-    type: 'Boolean')
+    type: 'Integer')
 ques8.save
     choice1_q8 = Choice.create(text: '1- SI', question:ques8)
         Outcome.create(choice: choice1_q8, career: literaruta)
@@ -116,7 +116,7 @@ ques8.save
 ques9 = Question.create(name: 'Pregunta N°9', 
     description:'Me atrae conocer los procesos y las áreas que hacen funcionar a las empresas.', 
     number: 9, 
-    type: 'Boolean')
+    type: 'Integer')
 ques9.save
     choice1_q9 = Choice.create(text: '1- SI', question:ques9)
         Outcome.create(choice: choice1_q9, career: economia)
@@ -125,7 +125,7 @@ ques9.save
 ques10 = Question.create(name: 'Pregunta N°10',
     description:'Me siento identificado con el pensamiento de algunos filósofos y escritores.'
     number: 10,
-    type: 'Boolean')
+    type: 'Integer')
 ques10.save
     choice_q10 = Choice.create(text: '1- SI', question:ques10)
         Outcome.create(choice: choice_q10, carrer: filosofia)
@@ -135,7 +135,7 @@ ques10.save
 ques11 = Question.create(name: 'Pregunta N°11',
     description:'Me encanta estudiar el cuerpo humano y conocer cómo funciona.Además, no me impresiona la sangre.'
     number: 11,
-    type: 'Boolean')
+    type: 'Integer')
 ques11.save
     choice_q11 = Choice.create(text: '1- SI', question:ques11)
         Outcome.create(choice: choice_q11, carrer: medicina)
@@ -144,9 +144,67 @@ ques11.save
 ques12 = Question.create(name: 'Pregunta N°12',
     description:'Si pudiera elegir, pasaría mucho tiempo diseñando novedosos objetos o edificios en mi computadora.'
     number: 12,
-    type: 'Boolean')
+    type: 'Integer')
 ques12.save
     choice_q12 = Choice.create(text: '1- SI', question:ques12)
-        Outcome.create(choice: choice_q12, carrer: computación)
+        Outcome.create(choice: choice_q12, carrer: computacion)
         Outcome.create(choice: choice_q12, carrer: arquitectura)
     choice_q12 = Choice.create(text: '2- NO', question:ques12)
+
+ques13 = Question.create(name : 'Pregunta°13',
+    description:'Si mi blog fuera temático, trataria de: ',
+    number: 13,
+    type: 'Integer')
+ques13.save
+    choice_q13 = Choice.create(text: '1- La importancia de la expresión artística en el desarollo de la identidad de los pueblos.', question:ques13)
+        Outcome.create(choice: choice_q13, carrer: sociologia)
+    choice_q13 = Choice.create(text: '2- La arqueología urbana como forma de aprender acerca de la historia cultural de una ciudad.', question:ques13)
+        Outcome.create(choice: choice_q13, carrer: historia)
+    choice_q13 = Choice.create(text: '3- La tecnología satelital en un proyecto para descubrir actividad volcánica para prevenir catástrofes climaticas.', question:ques13)
+        Outcome.create(choice: choice_q13, career: computacion)
+        Outcome.create(choice: choice_q13, career: geologia)
+    choice_q13 = Choice.create(text: '4- Ninguna de las opciones.', question:ques13)
+
+ques14 = Question.create(name : 'Pregunta°14',
+    description:'Integraría un equipo de trabajo encargado de producir un audiovisual sobre: ',
+    number: 14,
+    type: 'Integer')
+ques14.save
+    choice_q14 = Choice.create(text: '1- La práctica de deportes y su influencia positiva en el estado de ánimo de las personas.', question:ques14)
+        Outcome.create(choice: choice_q14, carrer: educación_fisica)
+    choice_q14 = Choice.create(text: '2- Los pensadores del siglo XX y su aporte a la gestión de las organizaciones.', question:ques14)
+        Outcome.create(choice: choice_q14, carrer: filosofia)
+        Outcome.create(choice: choice_q14, carrer: historia)
+    choice_q14 = Choice.create(text: '3- Las mascotas y su incidencia en la calidad de vida de las personas no videntes.', question:ques14)
+        Outcome.create(choice: choice_q14, carrer: veterinaria)
+    choice_q14 = Choice.create(text: '4- El uso de la PC como herramienta para el control de los procesos industriales.', question:ques14)
+        Outcome.create(choice: choice_q14, carrer: computacion)
+    choice_q14 = Choice.create(text: '5- Procesos productivos de una empresa.', question:ques14)
+        Outcome.create(choice: choice_q14, carrer: economia)
+    choice_q14 = Choice.create(text: '6- Ninguna de las opciones.', question:ques14)
+
+ques15 = Question.create(name : 'Pregunta°15',
+    description:'Sería importante destacarme como: ',
+    number: 15,
+    type: 'Integer')
+ques15.save
+    choice_q15 = Choice.create(text: '1- Director de una investigación técnico científica.', question:ques15)
+        Outcome.create(choice: choice_q15, carrer: fisica)
+        Outcome.create(choice: choice_q15, carrer: quimica)
+        Outcome.create(choice: choice_q15, carrer: computacion)
+    choice_q15 = Choice.create(text: '2- Gerente general de una empresa reconocida por su responsabilidad social.', question:ques15)
+        Outcome.create(choice: choice_q15, carrer: economia)
+    choice_q15 = Choice.create(text: '3- Un deportista famoso por su destreza física y su ética profesional.', question:ques15)
+        Outcome.create(choice: choice_q15, carrer: educación_fisica)
+    choice_q15 = Choice.create(text: '4- Experto en la detección precoz de enfermedades neurológicas en niños.', question:ques15)
+        Outcome.create(choice: choice_q15, carrer: medicina)
+    choice_q15 = Choice.create(text: '5- Un agente de prensa audaz, número uno en el ranking de notas a celebridades.', question:ques15)
+        Outcome.create(choice: choice_q15, carrer: periodismo)
+    choice_q15 = Choice.create(text: '6- Ninguna de las opciones.', question:ques15)
+
+
+
+
+
+
+
