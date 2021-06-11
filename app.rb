da01319f2ee203039834fcdb4a50c6f3d8afcdfa
Gaaston12 <gaastonmaartin12@gmail.com>
2021-06-11 16:00:33 -0300

    modification on app for GETS & POSTS

--- app.rb
+++ app.rb
@@ -5,41 +5,7 @@ class App < Sinatra::Base
     erb :landing
   end
 
-  post "/careers" do
-    data = JSON.parse request.body.read
-    career =  Career.new(name: data['name'])
-    
-    if career.save 
-      [201, {'location' => "careers/#{career.id}" }, 'CREATED']
-    else
-      [500,{}, 'Interal Server Error']
-    end
-  end
-
-  get "/careers" do
-    @careers = Career.all
-    erb :careers_index
-  end
-
-  get "/questions" do
-    erb :questions_index
-  end
-
-  get '/surveys' do
-    Survey.all.map{ |survey| survey.name }
-  end
-
-  post "/surveys" do
-    @survey = Survey.new(name: params[:name])
-
-    if @survey.save
-      @questions = Question.all
-      erb :surveys_index
-    else
-      [500, {}, 'Internal Server Error']
-    end
-  end
-
+  #POST & GET of posts
   post "/posts" do
     request.body.rewind  # in case someone already read it
     data = JSON.parse request.body.read
@@ -56,16 +22,87 @@ class App < Sinatra::Base
     p.description
   end
 
+
+  #POST & GET of career
+  post "/careers" do
+    career = Career.new(params[:name])
+    career.save
+    redirect "/careers"
+  end
+
+  get "/careers" do
+    @careers = Career.all
+    erb :careers_index
+  end
+
+  get "/careers/:id" do
+    "Carrera"
+  end
+
+    #POST & GET of questions
+  post '/questions' do
+    question = Question.new(name: params[:name])
+
+    if question.save
+      [201, {'Location' => "careers/#{career.id}"}, 'Created']
+    else
+      [500, {}, 'Internal Server Error']
+    end
+  end
+
+  get "/questions" do
+    @questions = Question.all
+    erb :questions_index
+  end
+
+
+  #POST & GET of surveys
+  post "/surveys" do
+    @user = Survey.new(username: params[:username])
+
+    if @user.save
+      @questions = Question.all
+      erb :responses_index
+    else
+      [500, {}, 'Internal Server Error']
+    end
+  end
+
+  get '/surveys' do
+    @survey = Survey.new(username: params[:username])
+    erb :surveys_index
+  end
+
+
+  #POST & GET of responses 
   post '/responses' do
-    survey = Survey.find(id: params[:survey_id])
-    params[:question_id].each do |q_id|
-      r = Response.new(choice_id: params[:"#{q_id}"], survey_id: survey.id, question_id: q_id)
-      r.save
+    @user = Survey.find(id: params[:survey_id])  
+
+    params[:question_id].each do |question_id|
+      resActu = Response.new(survey_id: @user.id, question_id: question_id, chioce_id: params[:"#{question_id}"])
+      resACtu.save
     end
 
-    @career_result   = Career.find(id: survey.id_career_result(Career.all))
-    survey.career_id = @career_result
-    erb :outcome_index
+    res = {}
+
+    for career in Career.all
+      res[career.id] = 0
+    end
+
+    for response in @user.responses
+      choice = Choice.find(id: response.choice_id)
+
+      for outcome in choice.outcome
+        res[outcome.career_id] += res[outcome.career_id]
+      end
+    end
+    
+    resCar = res.key(res.values.max)
+    @career = Career.find(id: resCar)
+    @user.update(career_id: @career.id)
+
+    erb :outcomes_index
   end
+
 end
 
